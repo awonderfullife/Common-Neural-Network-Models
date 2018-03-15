09c64cd5834f816a9e86692cc5d5a1b92bfc8fc0
cooper <cooper.yi@outlook.com>
2018-03-15 23:19:15 +0800

    cnn

--- Mxnet/MINIST_recognition/model.py
+++ Mxnet/MINIST_recognition/model.py
@@ -1,8 +1,8 @@
 import mxnet as mx
 import logging
+logging.getLogger().setLevel(logging.DEBUG)  # logging to stdout
 
 mnist = mx.test_utils.get_mnist()
-logging.getLogger().setLevel(logging.DEBUG)
 
 class Classifier:
     def __init__(self, batch_size=128, learning_rate=0.01, data=mnist):
@@ -13,30 +13,35 @@ class Classifier:
         predict = self.build_model()
         self.mx_model = mx.mod.Module(symbol=predict, context=mx.gpu())
 
-
     def build_model(self):
-        input_data = mx.sym.var('data')
-        input_layer = mx.sym.flatten(data=input_data)
+        input_layer = mx.sym.var('data')
 
-        fc_layer1 = mx.sym.FullyConnected(data=input_layer, num_hidden=128)
-        ac1 = mx.sym.Activation(data=fc_layer1, act_type="relu")
+        conv_layer1 = mx.sym.Convolution(data=input_layer, kernel=(5,5), num_filter=20)
+        actv_layer1 = mx.sym.Activation(data=conv_layer1, act_type="relu")
+        pool_layer1 = mx.sym.Pooling(data=actv_layer1, pool_type="max", kernel=(2,2), stride=(2,2))
 
-        fc_layer2 = mx.sym.FullyConnected(data=ac1, num_hidden=64)
-        ac2 = mx.sym.Activation(data=fc_layer2, act_type="relu")
+        conv_layer2 = mx.sym.Convolution(data=pool_layer1, kernel=(5,5), num_filter=50)
+        actv_layer2 = mx.sym.Activation(data=conv_layer2, act_type="relu")
+        pool_layer2 = mx.sym.Pooling(data=actv_layer2, pool_type="max", kernel=(2,2), stride=(2,2))
 
-        fc_layer3 = mx.sym.FullyConnected(data=ac2, num_hidden=10)
-        predict = mx.sym.SoftmaxOutput(data=fc_layer3, name="softmax")
+        flatten_layer1 = mx.sym.flatten(data=pool_layer2)
+
+        fc_layer1 = mx.sym.FullyConnected(data=flatten_layer1, num_hidden=64)
+        actv_layer3 = mx.sym.Activation(data=fc_layer1, act_type="relu")
+
+        fc_layer2 = mx.sym.FullyConnected(data=actv_layer3, num_hidden=10)
+        predict = mx.sym.SoftmaxOutput(data=fc_layer2, name="softmax")
 
         return predict
     
     def train(self):
         self.mx_model.fit(
-            train_data=self.train_iter,
-            eval_data=self.val_iter,
+            self.train_iter,
+            self.val_iter,
             optimizer="sgd",
             optimizer_params={"learning_rate":0.1},
             eval_metric='acc',  # report variable
-            batch_end_callback=mx.callback.Speedometer(self.batch_size, 100), # log each 100 batch_size
+            batch_end_callback=mx.callback.Speedometer(self.batch_size, 100), # report each 100 batch_size
             num_epoch=10
         )
     
